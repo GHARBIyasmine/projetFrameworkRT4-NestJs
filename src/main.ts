f39fcd8945181e24ea4faaafd6c33f9398ac6246
GHARBIyasmine <yasmine.gharbi@insat.ucar.tn>
2024-01-24 21:02:17 +0100

    database global config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv'
 
+dotenv.config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService)
-  await app.listen(configService.get('APP_PORT'));
+  await app.listen(configService.get<number>('APP_PORT'));
 }
 bootstrap();
